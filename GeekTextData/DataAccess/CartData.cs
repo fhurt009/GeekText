24dae778bd7dc92d479f6a0d7129088591745300
Fidel Hurtado <fhurt009@fiu.edu>
2019-10-05 12:08:11 -0400

    Added Data Access Project
    Added Cart Model
    
    Added Cart Data Access
    
    Added Sql Data Access
    
    Added Cart Controller

---
+++ GeekTextData/DataAccess/CartData.cs
@@ -0,0 +1,25 @@
+﻿using GeekTextData.Models;
+using GeekTextData.Sql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeekTextData.DataAccess
+{
+    public class CartData
+    {
+        public List<CartModel> GetCartByUserId(int Id)
+        {
+            SqlDataAccess sql = new SqlDataAccess();
+
+            var p = new { Id = Id };
+
+            var output = sql.LoadData<CartModel, dynamic>("dbo.spGetUserCart", p, "GeekTextDB");
+
+            return output;
+        }
+
+    }
+}
