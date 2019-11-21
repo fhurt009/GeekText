8cc9ceef33a86d5d8d1d65c4ced422bb5f154a2f
roeyfarag@outlook.com <roeyfarag@outlook.com>
2019-11-20 20:20:59 -0500

    Back end functionality for Wishlist.

---
+++ GeekTextAPI/Controllers/WishListController.cs
@@ -0,0 +1,64 @@
+﻿using GeekTextData.DataAccess;
+using GeekTextData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace GeekTextAPI.Controllers
+{
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
+    public class WishListController : ApiController
+    {
+        [HttpPost]
+        [Route("api/wishList/create")]
+        public List<string> createWishList(int id, string wishListName)
+        {
+            WishListData data = new WishListData();
+
+            return data.createWishList(id, wishListName);
+        }
+
+        [HttpPost]
+        [Route("api/wishList/addItem")]
+        public List<string> addItemToWishList(int id, int bookId, string wishListName)
+        {
+            WishListData data = new WishListData();
+
+            return data.addItemToWishList(id, bookId, wishListName);
+        }
+
+        // POST: api/Cart
+        [HttpDelete]
+        [Route("api/wishList/removeItem")]
+        public List<string> removeItemFromWishList(int id, int bookId, string wishListName)
+        {
+            WishListData data = new WishListData();
+
+            return data.removeItemFromWishList(id, bookId, wishListName);
+        }
+
+        // POST: api/Cart
+        [HttpPost]
+        [Route("api/wishList/transferItem")]
+        public List<string> transferItemFromWishList(int id, int bookId, 
+            string wishListNameOrigin, string wishListNameDestination)
+        {
+            WishListData data = new WishListData();
+
+            return data.transferItemFromWishList(id, bookId, wishListNameOrigin, wishListNameDestination);
+        }
+
+        [HttpGet]
+        [Route("api/wishList/getItem")]
+        public List<WishListModel> getUserWishList(int id, string wishListName)
+        {
+            WishListData data = new WishListData();
+
+            return data.getUserWishList(id, wishListName);
+        }
+    }
+}
